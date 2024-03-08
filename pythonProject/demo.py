8df386c9627571ff76d75a3de693a84827aae685
Hiteshkarwa <hiteshkarwa8@gmail.com>
2024-03-08 16:40:23 +0530

    python first commit

---
+++ pythonProject/demo.py
@@ -0,0 +1,26 @@
+# saved_password = "123admin123"
+# user_password = input("Enter password")
+# print(saved_password == user_password)
+
+# Simple interest Calculator
+# SI = p * n  * r /100
+# principal = int(input("Enter the amount borrowed"))
+# years = float(input("Enter the period in years"))
+# rate = float(input("Enter rate of interest"))
+#
+# interest = principal * years * rate / 100
+#
+# print(type(interest))
+# print("Simple interest calculated is : " + str(interest))
+
+# F string
+# name = " hitesh "
+# print(f"hello {name}")
+
+# calculating BMI
+#  bmi = weight in kgs /height in m
+# weight = float(input("Enter weight in kgs"))
+# height = float(input("Enter height in meters"))
+# bmi = weight / (height * height)
+# print(f"BMI is {bmi}")
+
